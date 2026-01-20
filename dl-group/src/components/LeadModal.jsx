d7d5f876ca652ec42270c0e3ec0e0e6fd25b6d1e
lhbecalli <luizhenriquebecallidefaria@gmail.com>
2026-01-20 14:21:27 -0300

    Modified

--- dl-group/src/components/LeadModal.jsx
+++ dl-group/src/components/LeadModal.jsx
@@ -92,7 +92,7 @@ export default function LeadModal({ isOpen, onClose }) {
 
           {/* FORMULÁRIO PRINCIPAL */}
           {(status === "idle" || status === "sending") && (
-            <form onSubmit={hannpdleSubmit} className="flex flex-col gap-5">
+           <form onSubmit={handleSubmit} className="flex flex-col gap-5">
               
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <input name="nome" type="text" placeholder="Seu nome" required className="bg-slate-800/50 border border-white/5 rounded-xl p-4 text-white placeholder-slate-500 outline-none focus:border-blue-500 transition-all focus:ring-1 focus:ring-blue-500/20" />
